fc284d1f8e35172547754fb91008da6caaa7ec0f
zlko23 <zlko@outlook.com>
2025-04-06 15:35:09 -0300

    GIT PUSH 	new file:   .gitignore 	new file:   complejidad.js 	modified:   package.json 	modified:   tarea.js 	modified:   test.js

--- tarea.js
+++ tarea.js
@@ -1,99 +1,75 @@
+const { Minima, Media, Maxima } = require("./complejidad");
+
 class Tarea {
-  constructor(codigo, duracion, complejidad) {
+  constructor(codigo, duracion) {
     this.codigo = codigo;
     this.duracion = duracion;
-    this.complejidad = complejidad;
+    this.complejidad = new Minima();
   }
+
   getDuracion() {
     return this.duracion;
   }
+
+  cambiarComplejidad() {
+    this.complejidad = this.complejidad.siguiente();
+  }
+
   getCosto() {
-    return this.duracion <= 5 ? 100 : 200;
+    if (this.complejidad.complejidad() === "minima") {
+      return 100 * this.getDuracion();
+    } 
+    else if (this.complejidad.complejidad() === "media") {
+      return this.getDuracion() >= 5
+        ? 200 * this.getDuracion()
+        : 150 * this.getDuracion();
+    } 
+    else if (this.complejidad.complejidad() === "maxima") {
+      return 300 * this.getDuracion() + 500;
+    }
+    return 0;
   }
-  getCostoTotal(){
-    let costoTotal = this.complejidad === "minima" ? 100 * this.getDuracion() :
-      this.complejidad === "media" ? (this.getDuracion() >= 5 ? 200 * this.getDuracion() : 150 * this.getDuracion()) :
-      this.complejidad === "maxima" ? 300 * this.getDuracion() + 500 : 0;
-    return costoTotal;
-  }
+
 }
 
 class TareaCompuesta {
-  constructor(codigo, duracion, subtareas = [], complejidad) {
+  constructor(codigo, duracion, subtareas = []) {
     this.codigo = codigo;
     this.duracion = duracion;
     this.subtareas = subtareas;
-    this.complejidad = complejidad;
+    this.complejidad = new Minima();
   }
+
   agregar(unaTarea) {
     this.subtareas.push(unaTarea);
   }
+
   getDuracion() {
     return this.subtareas.reduce(
       (acum, tarea) => acum + tarea.getDuracion(),
       this.duracion,
     );
   }
+
+  cambiarComplejidad() {
+    this.complejidad = this.complejidad.siguiente();
+  }
+
   getCosto() {
-    return this.duracion <= 5 ? 100 : 200;
-  }
-  getCostoTotal(){
-    let costoTotal = this.complejidad === "minima" ? 100 * this.getDuracion() :
-      this.complejidad === "media" ? (this.getDuracion() >= 5 ? 200 * this.getDuracion() : 150 * this.getDuracion()) :
-      this.complejidad === "maxima" ? 300 * this.getDuracion() + 500 * this.subtareas.length : 0;
-    costoTotal += this.subtareas.reduce((acum, tarea) => acum + tarea.getCostoTotal(), 0);
-    return costoTotal;
-  }
-}
+    let costo = 0;
 
-const t131 = new Tarea("1.3.1", 2 , "minima");
-const t132 = new Tarea("1.3.2", 1 , "minima");
-const t13 = new TareaCompuesta("1.3", 3, [t131, t132], "maxima");
-
-const t1221 = new Tarea("1.2.2.1", 3, "minima");
-const t1222 = new Tarea("1.2.2.2", 6, "media");
-const t121 = new Tarea("1.2.1", 4, "minima");
-const t122 = new TareaCompuesta("1.2.2", 1, [t1221, t1222], "minima");
-const t12 = new TareaCompuesta("1.2", 4, [t121, t122], "maxima");
-
-const t11 = new Tarea("1.1", 6, "maxima");
-
-const t1 = new TareaCompuesta("1", 2, [t11, t12, t13], "media");
-
-console.log(`\t_________________________________________________________________
-\t|  TAREA\tDURACION\tCOSTE\t\tCOMPLEJIDAD\t|
-\t|_______________________________________________________________|
-\t|  ${t1.codigo}\t\t${t1.duracion} minutos \t$ ${t1.getCostoTotal()}\t\t${t1.complejidad}\t\t|
-\t|  ${t11.codigo}\t\t${t11.duracion} minutos \t$ ${t11.getCostoTotal()}\t\t${t11.complejidad}\t\t|
-\t|  ${t12.codigo}\t\t${t12.duracion} minutos \t$ ${t12.getCostoTotal()}\t\t${t12.complejidad}\t\t|
-\t|  ${t121.codigo} \t${t121.duracion} minutos \t$ ${t121.getCostoTotal()}\t\t${t121.complejidad}\t\t|
-\t|  ${t122.codigo} \t${t122.duracion} minutos \t$ ${t122.getCostoTotal()}\t\t${t122.complejidad}\t\t|
-\t|  ${t1221.codigo} \t${t1221.duracion} minutos \t$ ${t1221.getCostoTotal()}\t\t${t1221.complejidad}\t\t|
-\t|  ${t1222.codigo} \t${t1222.duracion} minutos \t$ ${t1222.getCostoTotal()}\t\t${t1222.complejidad}\t\t|
-\t|  ${t13.codigo}\t\t${t13.duracion} minutos \t$ ${t13.getCostoTotal()}\t\t${t13.complejidad}\t\t|
-\t|  ${t131.codigo} \t${t131.duracion} minutos \t$ ${t131.getCostoTotal()}\t\t${t131.complejidad}\t\t|
-\t|  ${t132.codigo} \t${t132.duracion} minutos \t$ ${t132.getCostoTotal()}\t\t${t132.complejidad}\t\t|
-\t|_______________________________________________________________|
-\t|  SUBTOTAL\t${t1.getDuracion()} minutos \t$ ${t1.getCostoTotal()}\t\t\t\t|
-\t|_______________________________________________________________|`);
-
-class Proyecto {
-  constructor(tareas = []) {
-    this.tareas = tareas;
-  }
-  getDuracion() {
-    return this.tareas.reduce((total, tarea) => total + tarea.getDuracion(), 0);
-  }
-  getCostoTotal() {
-    return this.tareas.reduce(
-      (total, tarea) => total + tarea.getCostoTotal(),
-      0,
-    );
+    if (this.complejidad.complejidad() === "minima") {
+      costo = 100 * this.duracion;
+    } 
+    else if (this.complejidad.complejidad() === "media") {
+      costo =
+        this.duracion >= 5 ? 200 * this.duracion : 150 * this.duracion;
+    } 
+    else if (this.complejidad.complejidad() === "maxima") {
+      costo = 300 * this.duracion + 500 * this.subtareas.length;
+    }
+    return costo;
   }
 }
 
-const proyecto = new Proyecto([t1]);
-
-console.log(`\t|_______________________________________________________________|
-\t|  TOTAL\t${proyecto.getDuracion()} minutos \t$ ${proyecto.getCostoTotal()}\t\t\t\t|
-\t|_______________________________________________________________|`);
+module.exports = { Tarea, TareaCompuesta };
--- test.js
+++ test.js
@@ -6,15 +6,21 @@ const { Tarea, TareaCompuesta } = require("./tarea");
   t13.cambiarComplejidad();
   t13.cambiarComplejidad();
   
-  const t1221 = new Tarea("1.2.2.1", 3, "minima");
-  const t1222 = new Tarea("1.2.2.2", 6, "media");
+  const t1221 = new Tarea("1.2.2.1", 3);
+  const t1222 = new Tarea("1.2.2.2", 6);
+  t1222.cambiarComplejidad();
   const t121 = new Tarea("1.2.1", 4, "minima");
-  const t122 = new TareaCompuesta("1.2.2", 1, [t1221, t1222], "minima");
-  const t12 = new TareaCompuesta("1.2", 4, [t121, t122], "maxima");
+  const t122 = new TareaCompuesta("1.2.2", 1, [t1221, t1222]);
+  const t12 = new TareaCompuesta("1.2", 4, [t121, t122]);
+  t12.cambiarComplejidad();
+  t12.cambiarComplejidad();
   
-  const t11 = new Tarea("1.1", 6, "maxima");
+  const t11 = new Tarea("1.1", 6);
+  t11.cambiarComplejidad();
+  t11.cambiarComplejidad();
   
-  const t1 = new TareaCompuesta("1", 2, [t11, t12, t13], "media");
+  const t1 = new TareaCompuesta("1", 2, [t11, t12, t13]);
+  t1.cambiarComplejidad();
 
   
   function calcularCostoTotal(tarea) {
@@ -36,24 +42,7 @@ const { Tarea, TareaCompuesta } = require("./tarea");
     }
     return tarea.getDuracion();
   }
-  
-  console.log(`\t_________________________________________________________________
-  \t|  TAREA\tDURACION\tCOSTE\t\tCOMPLEJIDAD\t|
-  \t|_______________________________________________________________|
-  \t|  ${t1.codigo}\t\t${t1.duracion} minutos \t$ ${t1.getCosto()}\t\t${t1.complejidad()}\t\t|
-  \t|  ${t11.codigo}\t\t${t11.duracion} minutos \t$ ${t11.getCosto()}\t\t${t11.complejidad}\t\t|
-  \t|  ${t12.codigo}\t\t${t12.duracion} minutos \t$ ${t12.getCosto()}\t\t${t12.complejidad}\t\t|
-  \t|  ${t121.codigo} \t${t121.duracion} minutos \t$ ${t121.getCosto()}\t\t${t121.complejidad}\t\t|
-  \t|  ${t122.codigo} \t${t122.duracion} minutos \t$ ${t122.getCosto()}\t\t${t122.complejidad}\t\t|
-  \t|  ${t1221.codigo} \t${t1221.duracion} minutos \t$ ${t1221.getCosto()}\t\t${t1221.complejidad}\t\t|
-  \t|  ${t1222.codigo} \t${t1222.duracion} minutos \t$ ${t1222.getCosto()}\t\t${t1222.complejidad}\t\t|
-  \t|  ${t13.codigo}\t\t${t13.duracion} minutos \t$ ${t13.getCosto()}\t\t${t13.complejidad}\t\t|
-  \t|  ${t131.codigo} \t${t131.duracion} minutos \t$ ${t131.getCosto()}\t\t${t131.complejidad}\t\t|
-  \t|  ${t132.codigo} \t${t132.duracion} minutos \t$ ${t132.getCosto()}\t\t${t132.complejidad}\t\t|
-  \t|_______________________________________________________________|
-  \t|  SUBTOTAL\t${calcularDuracionTotal(t1)} minutos \t$ ${calcularCostoTotal(t1)}\t\t\t\t|
-  \t|_______________________________________________________________|`);
-  
+
   class Proyecto {
     constructor(tareas = []) {
       this.tareas = tareas;
@@ -69,6 +58,24 @@ const { Tarea, TareaCompuesta } = require("./tarea");
   }
   
   const proyecto = new Proyecto([t1]);
+  
+  console.log(`\t_________________________________________________________________
+  \t|  TAREA\tDURACION\tCOSTE\t\tCOMPLEJIDAD\t|
+  \t|_______________________________________________________________|
+  \t|  ${t1.codigo}\t\t${t1.duracion} minutos \t$ ${t1.getCosto()}\t\t${t1.complejidad.complejidad()}\t\t|
+  \t|  ${t11.codigo}\t\t${t11.duracion} minutos \t$ ${t11.getCosto()}\t\t${t11.complejidad.complejidad()}\t\t|
+  \t|  ${t12.codigo}\t\t${t12.duracion} minutos \t$ ${t12.getCosto()}\t\t${t12.complejidad.complejidad()}\t\t|
+  \t|  ${t121.codigo} \t${t121.duracion} minutos \t$ ${t121.getCosto()}\t\t${t121.complejidad.complejidad()}\t\t|
+  \t|  ${t122.codigo} \t${t122.duracion} minutos \t$ ${t122.getCosto()}\t\t${t122.complejidad.complejidad()}\t\t|
+  \t|  ${t1221.codigo} \t${t1221.duracion} minutos \t$ ${t1221.getCosto()}\t\t${t1221.complejidad.complejidad()}\t\t|
+  \t|  ${t1222.codigo} \t${t1222.duracion} minutos \t$ ${t1222.getCosto()}\t\t${t1222.complejidad.complejidad()}\t\t|
+  \t|  ${t13.codigo}\t\t${t13.duracion} minutos \t$ ${t13.getCosto()}\t\t${t13.complejidad.complejidad()}\t\t|
+  \t|  ${t131.codigo} \t${t131.duracion} minutos \t$ ${t131.getCosto()}\t\t${t131.complejidad.complejidad()}\t\t|
+  \t|  ${t132.codigo} \t${t132.duracion} minutos \t$ ${t132.getCosto()}\t\t${t132.complejidad.complejidad()}\t\t|
+  \t|_______________________________________________________________|
+  \t|  SUBTOTAL\t${calcularDuracionTotal(t1)} minutos \t$ ${calcularCostoTotal(t1)}\t\t\t\t|
+  \t|_______________________________________________________________|`);
+  
   
   console.log(`\t|  TOTAL\t${proyecto.getDuracion()} minutos \t$ ${proyecto.getCostoTotal()}\t\t\t\t|
   \t|_______________________________________________________________|`);
